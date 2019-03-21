a89fc3ce5a7d1067c3bf4effe7addd4069cecd94
Kas <kazaakas@hotmail.com>
2019-03-21 09:59:59 +0100

    make xhr property available on throwing version as well

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var xhrWrapper = require('./xhr');
 
 module.exports = function xhrRequest(args) {
-	return xhrWrapper(args).then(function(resp) {
+	var promise = xhrWrapper(args);
+	
+	var ret = promise.then(function(resp) {
 		if (resp.status < 200 || resp.status > 299) {
 			throw resp;
 		}
 	});
+
+	ret.xhr = promise.xhr;
+
+	return ret;
 };
